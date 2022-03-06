3772fb6bd14e44e901d09a4ffd5bd36039c47e1f
glassmonkey <peace.infi0407@gmail.com>
2022-03-07 00:52:45 +0900

    feat: can build

--- cmd/seimei/main.go
+++ cmd/seimei/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"github.com/glassmonkey/seimei"
+	"os"
+)
 
 func main() {
-	fmt.Printf("hello world")
+	name := flag.String("name", "", "separate full name(ex. 田中太郎)")
+	parse := flag.String("parse", " ", "separate characters")
+
+	flag.Parse()
+	if err := seimei.Run(*name, *parse); err != nil {
+		fmt.Printf("raised error: %s\n", err)
+		os.Exit(1)
+	}
 }

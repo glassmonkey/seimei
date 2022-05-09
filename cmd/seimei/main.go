3f44eef633993b472290ef37fd945d833953d8ff
gatchan0807 <yuki.dolphin.koga@gmail.com>
2022-05-10 01:25:55 +0900

    feat: add -file option

--- cmd/seimei/main.go
+++ cmd/seimei/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/glassmonkey/seimei"
 )
@@ -16,6 +17,7 @@ var (
 func main() {
 	name := flag.String("name", "", "separate full name(ex. 田中太郎)")
 	parse := flag.String("parse", " ", "separate characters")
+	file := flag.String("file", "", "path to text file of separated by break line undivided name list (ex. /tmp/undivided_name.txt)")
 	flag.Usage = func() {
 		_, err := fmt.Fprintf(flag.CommandLine.Output(), "Usage of seimei(%s-%s):\n", Version, Revision)
 		if err != nil {
@@ -25,6 +27,27 @@ func main() {
 		flag.PrintDefaults()
 	}
 	flag.Parse()
+
+	if *file != "" {
+		data, err := os.ReadFile(*file)
+
+		if err != nil {
+			fmt.Printf("raised error: %s\n", err)
+			os.Exit(1)
+		}
+
+		trimmed := strings.TrimSpace(string(data))
+		undividedNames := strings.Split(trimmed, "\n")
+
+		for index, name := range undividedNames {
+			if err := seimei.Run(os.Stdout, name, *parse); err != nil {
+				fmt.Printf("[Line:%d] raised error : %s\n", index+1, err)
+				os.Exit(1)
+			}
+		}
+
+		return
+	}
 
 	if err := seimei.Run(os.Stdout, *name, *parse); err != nil {
 		fmt.Printf("raised error: %s\n", err)

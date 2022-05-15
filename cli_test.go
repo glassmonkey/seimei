7a1fe6f8a5019edbbdc30a5866fd433578794a83
glassmonkey <peace.infi0407@gmail.com>
2022-05-16 02:08:02 +0900

    test: modify change testing with cobra in name parse testing

--- cli_test.go
+++ cli_test.go
@@ -4,60 +4,68 @@ import (
 	"bytes"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
-
 	"github.com/glassmonkey/seimei"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestSetFlagForName(t *testing.T) {
 	type testdata struct {
-		name            string
-		input           []string
-		wantName        seimei.Name
-		wantParseString seimei.ParseString
-		wantErrMsg      string
+		name       string
+		input      []string
+		wantOut    string
+		wantErrOut string
+		wantErrMsg string
 	}
 
 	tests := []testdata{
 		{
-			name:            "基本",
-			input:           []string{"-name", "田中太郎"},
-			wantName:        "田中太郎",
-			wantParseString: " ",
+			name:    "基本",
+			input:   []string{"--name", "田中太郎"},
+			wantOut: "田中 太郎\n",
 		},
 		{
-			name:            "パース文字指定",
-			input:           []string{"-name", "田中太郎", "-parse", "@"},
-			wantName:        "田中太郎",
-			wantParseString: "@",
+			name:    "パース文字指定",
+			input:   []string{"--name", "田中太郎", "--parse", "@"},
+			wantOut: "田中@太郎\n",
 		},
 		{
-			name:            "--2個でも良い",
-			input:           []string{"--name", "田中太郎"},
-			wantName:        "田中太郎",
-			wantParseString: " ",
+			name:    "短縮指定でも良い",
+			input:   []string{"-n", "田中太郎"},
+			wantOut: "田中 太郎\n",
 		},
 		{
 			name:       "指定がない",
-			input:      []string{"-name"},
-			wantErrMsg: "name command parse error: flag needs an argument: -name",
+			input:      []string{"--name"},
+			wantErrMsg: "flag needs an argument: --name",
 		},
 		{
 			name:       "未定義のパラメータ利用",
 			input:      []string{"--name", "田中太郎", "-x"},
-			wantErrMsg: "name command parse error: flag provided but not defined: -x",
+			wantErrMsg: "unknown shorthand flag: 'x' in -x",
+		},
+		{
+			name:       "未定義のパラメータ利用",
+			input:      []string{"--name", "田中太郎", "--any"},
+			wantErrMsg: "unknown flag: --any",
 		},
 		{
 			name:       "空",
 			input:      []string{},
-			wantErrMsg: "-name must be required (ex. 田中太郎)",
+			wantErrMsg: "required flag(s) \"name\" not set",
 		},
 	}
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			gotName, gotParseString, gotErr := seimei.SetFlagForName(tt.input)
+			stdout := &bytes.Buffer{}
+			stderr := &bytes.Buffer{}
+			sut := seimei.BuildNameCmd()
+			sut.SetOut(stdout)
+			sut.SetErr(stderr)
+			sut.SetArgs(tt.input)
+
+			_, gotErr := sut.ExecuteC()
 			if tt.wantErrMsg == "" && gotErr != nil {
 				t.Fatalf("happen error: %v", gotErr)
 			}
@@ -65,16 +73,16 @@ func TestSetFlagForName(t *testing.T) {
 				if gotErr == nil {
 					t.Fatal("happen no error")
 				}
-				if diff := cmp.Diff(tt.wantErrMsg, gotErr.Error()); diff != "" {
+				if diff := cmp.Diff(gotErr.Error(), tt.wantErrMsg); diff != "" {
 					t.Fatalf("failed to test on error. diff: %s", diff)
 				}
+				return
 			}
-
-			if gotName != tt.wantName {
-				t.Errorf("failed to test. got: %s, want: %s", gotName, tt.wantName)
+			if diff := cmp.Diff(stdout.String(), tt.wantOut); diff != "" {
+				t.Errorf("failed to test on error. diff: %s", diff)
 			}
-			if gotParseString != tt.wantParseString {
-				t.Errorf("failed to test. got: %s, want: %s", gotParseString, tt.wantParseString)
+			if diff := cmp.Diff(stderr.String(), tt.wantErrOut); diff != "" {
+				t.Errorf("failed to test on error. diff: %s", diff)
 			}
 		})
 	}
